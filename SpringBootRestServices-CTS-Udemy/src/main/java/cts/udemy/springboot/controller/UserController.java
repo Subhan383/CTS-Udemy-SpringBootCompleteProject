487eaed503ff7c10f2fe180d1ad68c7fc75bcf82
Subhan383 <abdulsubhan0383@gmail.com>
2020-08-24 12:18:27 +0530

    Added Exception Handling Mechanism

--- SpringBootRestServices-CTS-Udemy/src/main/java/cts/udemy/springboot/controller/UserController.java
+++ SpringBootRestServices-CTS-Udemy/src/main/java/cts/udemy/springboot/controller/UserController.java
@@ -1,8 +1,12 @@
 package cts.udemy.springboot.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,41 +14,63 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import cts.udemy.springboot.entity.User;
+import cts.udemy.springboot.exceptions.UserExistsException;
+import cts.udemy.springboot.exceptions.UserNotFoundException;
 import cts.udemy.springboot.service.UserService;
 
 @RestController
 public class UserController {
-	
+
 	@Autowired
 	UserService service;
-	
+
 	@GetMapping("/users")
-	public List<User> getAllUsers(){
+	public List<User> getAllUsers() {
 		return service.getAllUsers();
 	}
-	
+
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user) {
-		return service.createUser(user);
+	public ResponseEntity<Void> createUser(@RequestBody User user, UriComponentsBuilder uri) throws UserExistsException {
+		try {
+			service.createUser(user);
+			HttpHeaders header = new HttpHeaders();
+			header.setLocation(uri.path("/users/{id}").buildAndExpand(user.getId()).toUri());
+			return new ResponseEntity<Void>(header,HttpStatus.CREATED);
+		} catch (UserExistsException ex) {
+			throw new ResponseStatusException(HttpStatus.UPGRADE_REQUIRED, ex.getMessage());
+		}
+
 	}
 
 	@GetMapping("/users/{id}")
-	public User getUserByID(@PathVariable("id") int id) {
-		return service.getUserByID(id);
+	public Optional<User> getUserByID(@PathVariable("id") int id) throws UserNotFoundException {
+		try {
+			return service.getUserByID(id);
+		} catch (UserNotFoundException ex) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+		}
+
 	}
-	
+
 	@GetMapping("/users/byusername/{username}")
 	public User getUserByUserName(@PathVariable("username") String username) {
 		return service.getUserByUsername(username);
 	}
-	
+
 	@PutMapping("/users/{id}")
-	public User updateUser(@RequestBody User user,@PathVariable("id") int id) {
-		return service.updateUser(id, user);
+	public User updateUser(@RequestBody User user, @PathVariable("id") int id) throws UserNotFoundException {
+		try {
+			return service.updateUser(id, user);
+		} catch (UserNotFoundException ex) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage());
+		}
+
 	}
-	
+
 	@DeleteMapping("/users/{id}")
 	public void deleteUser(@PathVariable("id") int id) {
 		service.deleteUser(id);
--- SpringBootRestServices-CTS-Udemy/src/main/java/cts/udemy/springboot/service/UserService.java
+++ SpringBootRestServices-CTS-Udemy/src/main/java/cts/udemy/springboot/service/UserService.java
@@ -2,11 +2,16 @@ package cts.udemy.springboot.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import cts.udemy.springboot.entity.User;
+import cts.udemy.springboot.exceptions.UserExistsException;
+import cts.udemy.springboot.exceptions.UserNotFoundException;
 import cts.udemy.springboot.repository.UserRepository;
 
 @Service
@@ -24,25 +29,43 @@ public class UserService {
 		return users;
 	}
 
-	public User createUser(User user) {
+	public User createUser(User user) throws UserExistsException{
+		User existedUser=repo.findByUsername(user.getUsername());
+		if(existedUser!=null) {
+			throw new UserExistsException("User Already Existed, Please Select a Unique Username");
+		}
 		return repo.save(user);
 	}
 
-	public User getUserByID(int id) {
-		User user = repo.findById(id).get();
+	public Optional<User> getUserByID(int id) throws UserNotFoundException {
+		Optional<User> user = repo.findById(id);
+		if (!user.isPresent()) {
+			throw new UserNotFoundException("User not found in User DB");
+		}
 		return user;
 	}
-	
+
 	public User getUserByUsername(String username) {
 		return repo.findByUsername(username);
+
 	}
 
-	public User updateUser(int id,User user) {
+	@SuppressWarnings("unlikely-arg-type")
+	public User updateUser(int id, User user) throws UserNotFoundException {
 		user.setId(id);
+		if (!user.equals(id)) {
+			throw new UserNotFoundException("User not found in User DB, Please provide the correct UserID");
+		}
 		return repo.save(user);
 	}
 
+	@SuppressWarnings("unlikely-arg-type")
 	public void deleteUser(int id) {
+		Optional<User> user = repo.findById(id);
+		if (!user.equals(id)) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"User not found in User DB, Please provide the correct UserID");
+		}
 		repo.deleteById(id);
 
 	}
